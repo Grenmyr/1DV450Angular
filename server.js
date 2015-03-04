a6477d4f5f5c8a592c487c0f1556663cb845f302
dav <dg222cs@student.lnu.se>
2015-03-04 15:44:58 +0100

    satt upp node server fixat in require i bower, härnäst använda require

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ var express        = require('express');
 var app            = express();
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
-
 // configuration ===========================================
 
 
 var port = process.env.PORT || 8080; // set our port
-// mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
 // get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json 
@@ -16,8 +14,8 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse applica
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(express.static(__dirname + '/app')); // set the static files location /public/img will be /img for users
-
+app.use(express.static(__dirname + '/app')); // Set app folder public
+//app.use(express.static(__dirname + '/requirejs')); // set require js available for users
 // routes ==================================================
 //require('./app/routes')(app); // pass our application into our routes
 app.get('*', function(req, res) {
